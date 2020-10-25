2a33d1ba0b0d1e2ddfd7618d221969aa4282e282
Malvina Kochkina <malvina.kochkina@instech.no>
2020-10-25 11:33:36 +0100

    solve morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,31 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const decodeLetter = letter => {
+    let morseLetter = letter
+    .replace(/11/g, '-')
+    .replace(/10/g, '.')
+    .replace(/0/g, '');
+    const key = Object.keys(MORSE_TABLE).find(item => item == morseLetter);
+    return MORSE_TABLE[key];
+}
+
 function decode(expr) {
-    // write your solution here
+    let decoded = '';
+    let decodedArray = [];
+    const words = expr.split('**********');
+    const wordsByLetters = words.map(word => word.match(/.{1,10}/g));
+
+    wordsByLetters.forEach((word, index) => {
+        decoded += word.reduce((str,letter) => 
+        { console.log(str, decodeLetter(letter));
+          return str + decodeLetter(letter)
+          }, '');
+        if(index<wordsByLetters.length-1) decoded +=' '
+  
+    });
+
+    return decoded;
 }
 
 module.exports = {
